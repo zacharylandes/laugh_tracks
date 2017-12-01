bbdb32f6b7611275c8fc7adce6371ff59dc040b4
ZacharyLandes <zach.g.landes@gmail.com>
2017-12-01 10:58:25 -0700

    showing comedians specials

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,11 @@
 class LaughTracksApp < Sinatra::Base
+
+
+get '/comedians' do
+  @comedians = Comedian.all
+  @average = Comedian.average(:age).to_i
+  @age = params[:age]
+  erb :"index"
+end
+
 end
